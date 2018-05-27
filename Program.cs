2307c26d0a66535de1a6ecf06f822ca3c0262d83
Zoheir Khonyagar <zoheirkhonyagar@gmail.com>
2018-05-28 00:08:37 +0430

    add print & readFromFile function

--- Program.cs
+++ Program.cs
@@ -13,19 +13,61 @@ namespace al_project
 
             int count = Convert.ToInt32(reader.ReadLine());
             
+            reader.Close();
+            
             count = count * count;
             
             int[,] sudoku = new int[count , count];
-            
+
+            readFromFile(path , ref sudoku);
+
+            print(sudoku);
+
+        }
+
+        static void print(int [,] arr)
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+
+            }
+        }
+
+        static void readFromFile(string path ,ref int[,] arr)
+        {
+            StreamReader reader = new StreamReader(path);
+
+            int count = Convert.ToInt32(reader.ReadLine());
+
+            string line;
+
+            int i = 0;
+
             while (!reader.EndOfStream)
             {
-                Console.WriteLine(reader.ReadLine());
+
+                line = reader.ReadLine();
+
+                string[] tmp = new string[count];
+
+                tmp = line.Split(' ');
+
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = Convert.ToInt32(tmp[j]);
+                }
+
+                i++;
+
             }
 
-            Console.WriteLine(sudoku.Length);
-            
             reader.Close();
-        
         }
     }
 }
